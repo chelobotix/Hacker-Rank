36518b346d3d5f98c9e569a3a1aa1e7514ec3ac3
Marcelo Alarcon <marceloalarconbarrenechea@gmail.com>
2022-11-02 15:30:40 -0400

    picking_numbers.rb added

---
+++ picking_numbers.rb
@@ -0,0 +1,20 @@
+def pickingNumbers(a)
+  # Write your code here
+  max = []
+  counter = 0
+  a.each do |elem|
+    counter = a.count(elem)
+    counter += a.count(elem + 1)
+
+    max.push(counter)
+    counter = 0
+  end
+  max.max
+
+end
+
+array1 = [1,1,2,2,4,4,5,5,5]
+array2 = [4, 6, 5, 3, 3, 1]
+p pickingNumbers(array1)
+p pickingNumbers(array2)
+
