6da8548bdac7245b4afaad2c94f86d24756c4eed
Marcelo Alarcon <marceloalarconbarrenechea@gmail.com>
2022-11-02 22:03:37 -0400

    done

--- climbing_the_leaderboard.rb
+++ climbing_the_leaderboard.rb
@@ -1,65 +1,38 @@
 def climbingLeaderboard(ranked, player)
   # Write your code here
+  # ensure uniq
+  ranked = ranked.uniq
 
-  #mapping raiting
-  counter = 1
-  aux = ranked[0]
-  array = []
-  leader_board = []
-  ranked.each do |number|
-    if number == aux
-      array.push(counter)
-    elsif number < aux
-      counter += 1
-      aux = number
-      array.push(counter)
-    end
-  end
-  ranking = 0
+  results = []
+  # point to the last spot
+  rank_ptr = ranked.size - 1
+
+  # two pointers moving around?
   player.each do |score|
-    if score < ranked.min
-      leader_board.push(array.max + 1)
-    elsif score >= ranked.max
-      leader_board.push(1)
-    else
-      index = 0
-      maximum_value = 10 ** 9
-      control = false
-      ranked.each_with_index do |elem, i|
-        if score == elem
-          leader_board.push(array[i])
-          control = false
-          break
-        elsif elem == ranked.max
-          next
-        else
-          closer = (elem - score)
-          if closer <= maximum_value && closer > 0
-            maximum_value = closer
-            index = i
-            control = true
-          elsif closer < 0
-            break
+      if score < ranked[rank_ptr]
+          # 1 based, actual spot is behind the one comparing
+          results.push(rank_ptr + 2)
+      else
+          # moving rank_ptr until it's actually bigger than score
+          while(ranked[rank_ptr] <= score && rank_ptr >= 0)
+              rank_ptr -= 1
           end
-        end
+          # ok find the one
+          results.push(rank_ptr + 2)
       end
-      if control
-        leader_board.push(array[index + 1])
-      end
-    end
-
   end
+  results
 
 
 
-  leader_board
+
 end
 
 ranked1 = [100,90,90,80]
 players_score1 = [70, 80, 105]
 ranked2 = [100,100,50,40,40,20,10]
 players_score2 = [5, 25, 50, 120]
-p climbingLeaderboard(ranked1, players_score1)
+climbingLeaderboard(ranked2, players_score2)
 
 
 
