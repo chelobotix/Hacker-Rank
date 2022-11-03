175fae736308c3836b8864b01070fe69bf67435d
Marcelo Alarcon <marceloalarconbarrenechea@gmail.com>
2022-11-02 21:50:08 -0400

    Work no timeout problem

--- climbing_the_leaderboard.rb
+++ climbing_the_leaderboard.rb
@@ -15,20 +15,43 @@ def climbingLeaderboard(ranked, player)
       array.push(counter)
     end
   end
-  aux = 0
-  player.each_with_index do |score, i|
-    if score >= ranked[0]
-      leader_board.push(array[0])
-    elsif score == ranked[-1]
-      leader_board.push(array[-1])
-    elsif score < ranked[-1]
+  ranking = 0
+  player.each do |score|
+    if score < ranked.min
       leader_board.push(array.max + 1)
+    elsif score >= ranked.max
+      leader_board.push(1)
     else
-      index = ranked.index(ranked.min_by{|x| (score-x).abs})
-      leader_board.push(array[index])
+      index = 0
+      maximum_value = 10 ** 9
+      control = false
+      ranked.each_with_index do |elem, i|
+        if score == elem
+          leader_board.push(array[i])
+          control = false
+          break
+        elsif elem == ranked.max
+          next
+        else
+          closer = (elem - score)
+          if closer <= maximum_value && closer > 0
+            maximum_value = closer
+            index = i
+            control = true
+          elsif closer < 0
+            break
+          end
+        end
+      end
+      if control
+        leader_board.push(array[index + 1])
+      end
     end
 
   end
+
+
+
   leader_board
 end
 
@@ -36,7 +59,7 @@ ranked1 = [100,90,90,80]
 players_score1 = [70, 80, 105]
 ranked2 = [100,100,50,40,40,20,10]
 players_score2 = [5, 25, 50, 120]
-p climbingLeaderboard(ranked2, players_score2)
+p climbingLeaderboard(ranked1, players_score1)
 
 
 
