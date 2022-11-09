48c5ed105d8d0eea0ebf0ae49b920e9a779902cd
Marcelo Alarcon <marceloalarconbarrenechea@gmail.com>
2022-11-09 16:31:32 -0400

    add save_the_prisoner.rb

--- save_the_prisoner.rb
+++ save_the_prisoner.rb
@@ -1,17 +1,10 @@
 def saveThePrisoner(n, m, s)
   # Write your code here
-  candy = 0
-  if m > n
-    m = m % n
+  sum = s + m - 1;
+  if sum % n == 0
+    return n
   end
-
-  if s + m - 1 <= n
-    candy = (s + m) - 1
-  else
-    candy = (m + s)-(n + 1)
-  end
-
-  candy
+  return sum % n;
 end
 
 p saveThePrisoner(31, 238250965, 2)
