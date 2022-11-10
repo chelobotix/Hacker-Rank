d12e1dfc9d5d04e61ad40ae8a82d7d9fb036a33a
Marcelo Alarcon <marceloalarconbarrenechea@gmail.com>
2022-11-10 12:11:35 -0400

    add circular_array_rotation.rb

--- circular_array_rotation.rb
+++ circular_array_rotation.rb
@@ -1,25 +1,7 @@
 def circularArrayRotation(a, k, queries)
   # Write your code here
 
-  (1..k).each do |i|
-    aux1 = a[-1]
-    aux2 = a[0]
-    final = a[-1]
-    a.each_with_index do |elem, index|
-      if index < a.length - 1
-        if index.even?
-          aux1 = a[index + 1]
-          a[index + 1] = aux2
-        else
-          aux2 = a[index + 1]
-          a[index + 1] = aux1
-        end
-      else
-        a[0] = final
-      end
-    end
-  end
-
+  a.rotate!(-k)
   q = []
   queries.each_with_index do |elem, index|
     q.push(a[elem])
